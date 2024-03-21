1caf5aa0797a0c9303cbd8931868fa18ed188917
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2024-03-22 06:02:12 +0900

    feat: Routerni qurib olish

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log("Web Serverni Boshlash");
 const express = require("express");
 const app = express();
+const router = require("./router");
 
 //  MongoDB chaqirish
 const db = require("./server").db();
@@ -18,5 +19,6 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 Routing codes
+app.use("/", router);
 
 module.exports = app;
---
+++ router.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", function (req, res) {
+    res.send("home sahifasidasiz");
+});
+
+router.get("/menu", (req, res) => {
+    res.send("Menu sahifasidasiz");
+});
+
+router.get("/community", (req, res) => {
+    res.send("Jamiyat sahifasidasiz");
+});
+module.exports = router;
