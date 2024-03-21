372c7dcff61401e3de3ac30e37fd60c652fc5b37
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2024-03-22 05:40:21 +0900

    feat: dotenv & project configuration

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const http = require("http");
-
 const mongodb = require("mongodb");
-
-let db;
-const connectionString =
-    "mongodb+srv://badboydev99:HCVf4tiIioL65lMy@cluster0.m1yvyll.mongodb.net/STORE?retryWrites=true&w=majority&appName=Cluster0";
+const connectionString = process.env.MONGO_URL;
 
 mongodb.connect(
     connectionString,
@@ -20,7 +19,7 @@ mongodb.connect(
 
             const app = require("./app");
             const server = http.createServer(app);
-            let PORT = 3000;
+            let PORT = process.env.PORT || 3000;
             server.listen(PORT, function () {
                 console.log(
                     `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
